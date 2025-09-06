1106f4093399a314c95d4ea6b1ff2ad48d623205
Michael Smith <m@michaelsmith.be>
2025-09-06 23:06:21 +0200

    Add setup instructions for ISO based installation and configuration
    Prefer ISO based installation over ready made VM images provided by FreeBSD.
    This gives us more flexibility and prevents annoyances like preconfigured disk partitions and sizes.

--- vm/run.sh
+++ vm/run.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
 
 qemu-system-x86_64 \
-  -M q35 \
   -m 4G \
   -cpu Haswell,-avx \
   -smp 4 \
   -machine q35,accel=tcg \
-  -drive file=FreeBSD-14.3-RELEASE-amd64.qcow2,format=qcow2,if=virtio \
-  -netdev user,id=net0,hostfwd=tcp::2222-:22 \
+  -drive file=FreeBSD-14.3-RELEASE-amd64.qcow2,format=qcow2 \
   -device e1000,netdev=net0 \
+  -netdev user,id=net0,hostfwd=tcp::2222-:22 \
   -nographic \
   -serial mon:stdio \
   -vga none \
