cca5b2dd924231c4a9af577110e47d90981d7e21
Michael Smith <m@michaelsmith.be>
2025-09-15 14:16:43 +0200

    Update VM run script to use kvm and host cpu

--- vm/run.sh
+++ vm/run.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 qemu-system-x86_64 \
-  -m 4G \
-  -cpu Haswell,-avx \
+  -m 2G \
+  -cpu host \
   -smp 4 \
-  -machine q35,accel=tcg \
+  -machine q35,accel=kvm \
   -drive file=FreeBSD-14.3-RELEASE-amd64.qcow2,format=qcow2 \
   -device e1000,netdev=net0 \
   -netdev user,id=net0,hostfwd=tcp::2222-:22 \
